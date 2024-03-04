d0b28bdf38d913ca700485773de2a1989a704861
garretvan <vandykegl4@gmail.com>
2024-03-04 13:15:09 -0500

    Add function now multiplies instead of add

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a * b
         
 def SayHello():
-        print("Hello CIS 411 - SAD")
+        print("Hello CIS 411")
 
 if __name__ == '__main__':
         SayHello()
