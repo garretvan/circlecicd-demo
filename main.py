f3c2c086716c267621eaf654f24423d0a1bbd1ff
garretvan <vandykegl4@gmail.com>
2024-03-04 11:00:40 -0500

    test commit again

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("Hello CIS 411")
+        print("Hello CIS 411 - SAD")
 
 if __name__ == '__main__':
         SayHello()
