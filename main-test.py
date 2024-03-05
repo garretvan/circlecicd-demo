cec81ee639bb4666366c5d14528fd1135d5748b6
garretvan <vandykegl4@gmail.com>
2024-03-05 08:18:19 -0500

    First commit

--- main-test.py
+++ main-test.py
@@ -4,6 +4,7 @@ from main import Add
 def TestAdd():
         assert Add(2,3) == 5
         print("Add Function works correctly")
+        print("Test")
 
 if __name__ == '__main__':
         TestAdd()
